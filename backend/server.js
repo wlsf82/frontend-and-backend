44a80d221fc5facbfe4ce99a3476af7ccac7edbb
Walmyr <wlsf82@gmail.com>
2022-02-22 21:57:24 +0100

    Update "db" data to have better samples

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ app.use(function(req, res, next) {
 
 const database = {
     customers: [
-        { id: 1, name: "Americas Inc.", employees: 10, contactInfo: { name: "John Smith", email: "jsmith@americasinc.com"}},
+        { id: 1, name: "Americas Inc.", employees: 100, contactInfo: { name: "John Smith", email: "jsmith@americasinc.com"}},
         { id: 2, name: "Caribian Airlnis", employees: 1000, contactInfo: { name: "Jose Martinez", email: "martines@cair.com"}},
         { id: 3, name: "MacroSoft", employees: 540, contactInfo: { name: "Bill Paxton", email: "bp@ms.com"}},
         { id: 4, name: "United Brands", employees: 20},
-        { id: 5, name: "Bananas Corp", employees: 10000, contactInfo: { name: "Xavier Hernandez", email: "xavier@bananas.com"}},
-        { id: 6, name: "XPTO.com", employees: 102, contactInfo: { name: "Daniel Zuck", email: "zuckh@xpto.com"}}
+        { id: 5, name: "Bananas Corp", employees: 1001, contactInfo: { name: "Xavier Hernandez", email: "xavier@bananas.com"}},
+        { id: 6, name: "XPTO.com", employees: 101, contactInfo: { name: "Daniel Zuck", email: "zuckh@xpto.com"}}
     ]
 };
 

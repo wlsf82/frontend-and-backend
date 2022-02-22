7d2d39a061c3f1b696b81779dad4ac1b934fa3e6
Walmyr <wlsf82@gmail.com>
2022-02-22 15:18:39 +0100

    Use testing-library for frontend unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, screen } from '@testing-library/react';  
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders the welcome screen', () => {
+  render(<App />);
+  expect(screen.getByText('Welcome to Customer App')).toBeInTheDocument();
 });

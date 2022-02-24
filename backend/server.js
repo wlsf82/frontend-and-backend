5e865537a36a32933d644aba22294f89b985f03b
Walmyr <wlsf82@gmail.com>
2022-02-24 03:33:48 +0100

    Fix indentation on server file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 const database = {
     customers: [
